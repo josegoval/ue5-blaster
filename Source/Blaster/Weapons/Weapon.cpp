d556d372e8262f1b2c29d41fd5c8e2c59f2064e8
josegoval <josegovalmusic@gmail.com>
2022-07-16 12:31:34 +0200

    feat(BP_Weapon): default Weapon blueprint that extends from AWeapon class

---
+++ Source/Blaster/Weapons/Weapon.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AWeapon::AWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	bReplicates = true;
+
+	// SkeletalMeshComponent configuration
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMeshComponent->SetupAttachment(RootComponent);
+	SetRootComponent(SkeletalMeshComponent);
+	SkeletalMeshComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	SkeletalMeshComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	SkeletalMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	// PickupAreaSphereComponent configuration
+	PickupAreaSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("PickupAreaSphere"));
+	PickupAreaSphereComponent->SetupAttachment(RootComponent);
+	PickupAreaSphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	PickupAreaSphereComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		PickupAreaSphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		PickupAreaSphereComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn,
+		                                                         ECollisionResponse::ECR_Overlap);
+	}
+}
---
+++ Source/Blaster/Weapons/Weapon.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Pickable UMETA(DisplayName = "Pickable"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+	
+	EWS_MAX UMETA(DisplayName = "Default MAX"),
+};
+
+UCLASS()
+class BLASTER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class USkeletalMeshComponent* SkeletalMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class USphereComponent* PickupAreaSphereComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	EWeaponState WeaponState;
+};
